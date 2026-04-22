4b3720e9879aa89192348be211f96c4515f199e9
ActivistChecklist <contact@activistchecklist.org>
2026-04-22 05:21:07 -0700

    Skip some slow tests on github actions

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 import path from 'path'
+
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true'
 
 export default defineConfig({
   test: {
     globals: true,
+    exclude: [
+      ...configDefaults.exclude,
+      ...(isGitHubActions ? ['__tests__/og-image.test.js'] : []),
+    ],
   },
   resolve: {
     alias: {
